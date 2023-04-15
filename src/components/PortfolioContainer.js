79890268f181fc5fb3de8ba06e8c3d730d3312ab
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-04-15 19:01:00 +0800

    Add outline for the header

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+export default function PortfolioContainer() {
+    return (
+        <header>
+            <Header />
+        </header>
+    )
+}
