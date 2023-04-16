2e25a1f15efdf585d90120cce79ff31840e147e9
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-04-16 19:25:28 +0800

    setup exports for pages

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-export default function() {
+export default function Home () {
     return (
-        <div>testing</div>
+        <div class="container">
+            <h1>Ryan McDiarmid</h1>
+            <h2>Full Stack Developer</h2>
+        </div>
     )
 }
