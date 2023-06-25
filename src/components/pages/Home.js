927bd931b0bf7ab5ee7977836d3a50fa2b0c72cd
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-06-25 19:51:56 +0800

    add about me info to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,6 +5,21 @@ export default function Home () {
         <div class="container">
             <h1>Ryan McDiarmid</h1>
             <h2>Full Stack Developer</h2>
+
+            <p>
+                Graduate soft-ware Developer looking for a career change into soft-ware development.
+            </p>
+
+            <p>
+                I'm looking for a more integrated role in developing applications from the ground up and 
+                learning how they work and interact with the user. I have experience using Shopify to manage products, 
+                customers and to create a website.
+            </p>
+
+            <p>
+                I'm always looking for new technologies and techniques to help me become a better developer, and I'm eager 
+                to explore unfamiliar territory to expand my skills.
+            </p>
         </div>
     )
 }
